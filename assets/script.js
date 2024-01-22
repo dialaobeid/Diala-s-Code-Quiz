245050b521a42f49fdbfa6eba84e6579827d3f73
dobeid <dialajo9@gmail.com>
2024-01-22 14:49:22 -0600

    assets folder including the js and css files. the js file has the questions/answers variable created while the css file remains blank for now

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// Creating the quiz questions and answers
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    { 
+        question: "The condition in an if / else statement is enclosed within ______.",
+        choices: ["Quotes", "Curly brackets", "Parantheses", "Square brackets"],
+        answer: "Parantheses"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ______.",
+        choices: ["Numbers and strings", "Other arrays ", "Booleans", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["Commas", "Curly brackets", "Quotes", "Parantheses"],
+        answer: "Quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "Terminal/bash", "For loops", "Console log"],
+        answer: "Javascript"
+    }
+];

ce8bceb3553887a1759abfdb18c496628b5e650a
dobeid <dialajo9@gmail.com>
2024-01-22 15:49:50 -0600

    completed checkAnswer function

--- assets/script.js
+++ assets/script.js
@@ -46,18 +46,34 @@ function displayQuestion() {
     document.getElementById("quiz-question").textContent = currentQuestion.question; // adds ques text on html
 
     // creates answer choice buttons
-    var choicesContainer = document.getElementById("answer-choices"); //
-    choicesContainer.innerHTML = "";
+    var choicesContainer = document.getElementById("answer-choices"); 
+    choicesContainer.innerHTML = "";    // this container represents the html element answer-choices, will display and clear each set of answer choices per ques
 
     currentQuestion.choices.forEach(
         function (choice, index) {
-        var button = document.createElement("button");
+        var button = document.createElement("button");        // creates a button for each answer choice
         button.textContent = (index + 1) + ". " + choice;     // sets answer choices in button text
         button.addEventListener("click", checkAnswer);        // when clicked, this event listener added to the button will trigger the checkAnswer function 
-        choicesContainer.appendChild(button);                 // appends button 
+        choicesContainer.appendChild(button);                 // appends button to choicesContainer
     });
 }
 
 // Function to check the selected answer
 function checkAnswer(event) {
+    var selectedAnswer = event.target.textContent.slice(3); // gets the answer from the button text
+    var currentQuestion = questions[currentQuestionIndex];  // adding this var into this fx as wells
+
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+        // then we have correct answer
+    } else {
+        // incorrect answer, time is deducted
+        startTime -= 10;                                    // Deducts 10s for each incorrect answer
+    }
+
+    currentQuestionIndex++;                                 // Moving onto the next ques or quiz ends
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+    } else {
+        endQuiz();
+    }
 }
